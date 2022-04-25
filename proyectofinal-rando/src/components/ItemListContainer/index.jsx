71bb9d01509ae8b293f08765fa3db9ca11f1f77a
Gonzalo Rando <randosgonzalo@gmail.com>
2022-04-24 23:03:03 -0300

    pre entrega desafio 4

--- proyectofinal-rando/src/components/ItemListContainer/index.jsx
+++ proyectofinal-rando/src/components/ItemListContainer/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 import './item-list-container.css'
 
-const ItemListContainer = ({ greeting }) => {
-  return (
-    <div className='containerItem'>{greeting}</div>
-  )
+function ItemListContainer ({ greeting }) {
+
+  function prueba() {
+    console.log('prueba prop function');
+  }
+
+return (
+  <div className='list-item-container'>
+    <ItemCount initial={0} stock={5} onAdd={prueba} />  
+  </div>
+)
 }
+
 
 export default ItemListContainer;
